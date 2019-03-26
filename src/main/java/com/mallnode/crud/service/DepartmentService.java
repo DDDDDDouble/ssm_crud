e1fc87d451f6df083822bf20a28ac3ec6e1ef256
DDDDDDouble <1475145313@qq.com>
2019-03-26 22:02:55 +0800

    16

---
+++ src/main/java/com/mallnode/crud/service/DepartmentService.java
@@ -0,0 +1,22 @@
+package com.mallnode.crud.service;
+
+import com.mallnode.crud.bean.Department;
+import com.mallnode.crud.dao.DepartmentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentMapper departmentMapper;
+
+	public List<Department> getDepts() {
+		// TODO Auto-generated method stub
+		List<Department> list = departmentMapper.selectByExample(null);
+		return list;
+	}
+
+}
---
+++ src/main/java/com/mallnode/crud/test/MBGTest.java
@@ -0,0 +1,25 @@
+package com.mallnode.crud.test;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MBGTest {
+
+    //可以使用这段代码生成dao，或者直接运行mybatis generator插件
+    public static void main(String[] args) throws Exception {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
